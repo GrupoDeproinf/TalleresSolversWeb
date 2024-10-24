9419cf78397787bcf19faf396db36363521ba73f
Gabriel <gabrielgonzalez.alexis@gmail.com>
2024-10-24 11:39:06 -0400

    Recuperar contraseña

--- src/configs/firebaseAssets.config.ts
+++ src/configs/firebaseAssets.config.ts
@@ -5,6 +5,7 @@ import { getStorage } from 'firebase/storage'
 import { getAuth } from 'firebase/auth'
 import { initializeFirestore } from 'firebase/firestore'
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -8,6 +8,12 @@ import type {
     SignUpResponse,
 } from '@/@types/auth'
 
+import { auth } from '@/configs/firebaseAssets.config';
+import { getAuth , sendPasswordResetEmail } from 'firebase/auth';
+import { FirebaseError } from 'firebase/app';
+
+
+
 export async function apiSignIn(data: SignInCredential) {
     return ApiService.fetchData<SignInResponse>({
         url: '/sign-in',
@@ -32,12 +38,32 @@ export async function apiSignOut() {
 }
 
 export async function apiForgotPassword(data: ForgotPassword) {
-    return ApiService.fetchData({
-        url: '/forgot-password',
-        method: 'post',
-        data,
-    })
+    const auth = getAuth();
+    const { email } = data;
+
+    console.log("Email recibido en el backend:", email);
+
+    try {
+        await sendPasswordResetEmail(auth, email);
+        return { success: true };
+    } catch (error) {
+        console.error("Error al enviar el correo: ", error);
+
+        // Asegúrate de que el error es de tipo FirebaseError
+        if (error instanceof FirebaseError) {
+            switch (error.code) {
+                case 'auth/invalid-email':
+                    throw new Error("El correo electrónico no es válido.");
+                // Puedes agregar otros casos según sea necesario
+                default:
+                    throw new Error("No se pudo enviar el correo de restablecimiento de contraseña. Por favor, verifica el correo electrónico e inténtalo de nuevo.");
+            }
+        } else {
+            throw new Error("Se produjo un error inesperado.");
+        }
+    }
 }
+
 
 export async function apiResetPassword(data: ResetPassword) {
     return ApiService.fetchData({
