ac6799b83d72d4b31f21719e83571bb92a298bd3
ManuelGoncalvesR <manuelgoncalveser@gmail.com>
2024-10-31 12:38:41 -0400

    ya se leen los servicios correctamente

--- src/views/pages/ProfileGarage/ProfileGarage.tsx
+++ src/views/pages/ProfileGarage/ProfileGarage.tsx
@@ -74,28 +74,31 @@ const ProfileGarage = () => {
             const docRef = doc(db, 'Usuarios', path);
             const resp = await getDoc(docRef);
             const dataFinal = resp.data() || null;
-
-            const servicesRef = collection(db, 'Servicios');
-            const servicesQuery = query(servicesRef, where('taller', '==', dataFinal?.nombre));
-            const servicesSnap = await getDocs(servicesQuery);
-            const services = servicesSnap.docs.map(doc => {
-                const data = doc.data();
-                return {
-                    nombre_servicio: data.nombre_servicio || '',
-                    descripcion: data.descripcion || '',
-                    precio: data.precio || '0',
-                    taller: data.taller || '',
-                    puntuacion: data.puntuacion || '0',
-                    uid_servicio: data.uid_servicio || '',
-                };
-            });
-
+    
+            // Extraer los IDs de los servicios desde el documento 'Usuarios'
+            const serviceIds = dataFinal?.servicios || [];
+    
+            // Obtener detalles completos de cada servicio usando sus IDs
+            const services = await Promise.all(
+                serviceIds.map(async (serviceId: string) => {
+                    const serviceDocRef = doc(db, 'Servicios', serviceId);
+                    const serviceDoc = await getDoc(serviceDocRef);
+                    const serviceData = serviceDoc.data();
+    
+                    return {
+                        uid_servicio: serviceId,
+                        nombre_servicio: serviceData?.nombre_servicio || '',
+                        descripcion: serviceData?.descripcion || '',
+                        precio: serviceData?.precio || '0',
+                        taller: serviceData?.taller || '',
+                        puntuacion: serviceData?.puntuacion || '0'
+                    };
+                })
+            );
+    
             setData(dataFinal);
-            setServices(services);
-            setPlanes(planes)
-
-            console.log("aqui la data ", dataFinal, services)// Estado para los servicios
-
+            setServices(services); // Estado actualizado con la información completa de cada servicio
+    
             setFormData({
                 nombre: dataFinal?.nombre || '',
                 logoUrl: dataFinal?.logoUrl || '',
@@ -114,6 +117,7 @@ const ProfileGarage = () => {
             setLoading(false);
         }
     };
+    
 
     useEffect(() => {
         getData();
