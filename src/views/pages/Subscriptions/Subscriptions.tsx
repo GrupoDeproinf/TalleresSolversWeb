fe3033d5aaf4f4bc304d5d22223946ac19184a76
Anthonychacon2412 <chaconanthony.la@gmail.com>
2024-11-06 13:52:21 -0400

    subscripcion

--- src/views/pages/Subscriptions/Subscriptions.tsx
+++ src/views/pages/Subscriptions/Subscriptions.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
+import {
+    collection,
+    doc,
+    getDocs,
+    Timestamp,
+    updateDoc,
+} from 'firebase/firestore'
 import { db } from '@/configs/firebaseAssets.config'
 import Table from '@/components/ui/Table'
 import {
@@ -12,20 +18,30 @@ import {
     ColumnDef,
 } from '@tanstack/react-table'
 import { Row } from '@tanstack/react-table'
+import {
+    Drawer,
+    Pagination,
+    Button,
+    Switcher,
+    toast,
+    Notification,
+} from '@/components/ui'
+import { FaCheckCircle, FaExclamationCircle, FaRegEye } from 'react-icons/fa'
 
 interface Subscripcion {
     nombre?: string
     status?: string
     monto: string
-    fecha_inicio?: string
-    fecha_fin?: string
+    vigencia: string
+    fecha_inicio: Timestamp
+    fecha_fin: Timestamp
     proximo_pago?: string
 }
 
 interface Usuario {
     id: string
     nombre: string
-    subscripcion_actual?: Subscripcion
+    subscripcion_actual?: Subscripcion | null
 }
 
 const UsuariosComponent = () => {
@@ -34,6 +50,14 @@ const UsuariosComponent = () => {
     const [error, setError] = useState<string | null>(null)
     const [sorting, setSorting] = useState<any[]>([])
     const [filtering, setFiltering] = useState<ColumnFiltersState>([])
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false)
+    const [selectedPerson, setSelectedPerson] = useState<{
+        usuario: Usuario | null
+        subscripcion: Subscripcion | null
+    }>({
+        usuario: null,
+        subscripcion: null,
+    })
     const [currentPage, setCurrentPage] = useState(1)
     const rowsPerPage = 6
 
@@ -49,14 +73,16 @@ const UsuariosComponent = () => {
                     }),
                 ) as Usuario[]
 
-                // Filtrar solo usuarios con subscripción actual
+                // Verifica las fechas de los usuarios
+                console.log('Usuarios con fechas:', usuariosData)
+
                 const usuariosConSubscripcion = usuariosData.filter(
                     (usuario) => usuario.subscripcion_actual,
                 )
                 setUsuarios(usuariosConSubscripcion)
             } catch (error) {
                 console.error('Error fetching usuarios:', error)
-                setError('Error al cargar los usuarios')
+                setError('Error al cargar las subscripciones')
             } finally {
                 setLoading(false)
             }
@@ -64,6 +90,116 @@ const UsuariosComponent = () => {
 
         fetchUsuarios()
     }, [])
+    // Solo se ejecuta una vez cuando el componente se monta
+
+    const openDrawer = (usuario: Usuario, subscripcion: Subscripcion) => {
+        setSelectedPerson({ usuario, subscripcion })
+        setDrawerIsOpen(true)
+    }
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false)
+        setSelectedPerson({ usuario: null, subscripcion: null })
+    }
+
+    const handleSaveChanges = async () => {
+        if (selectedPerson.usuario?.id && selectedPerson.subscripcion) {
+            try {
+                // Verificar si el estado de la suscripción es "Por Aprobar"
+                if (selectedPerson.subscripcion.status === 'Por Aprobar') {
+                    // Comprobar si la suscripción está vigente (es decir, si ya tiene una fecha_fin definida)
+                    const fechaFin = selectedPerson.subscripcion.fecha_fin
+                        ? selectedPerson.subscripcion.fecha_fin.toDate() // Convertir Timestamp a Date
+                        : null
+                    const fechaActual = new Date()
+
+                    if (fechaFin && fechaActual < fechaFin) {
+                        // Si la suscripción está vigente, no permitir la actualización
+                        toast.push(
+                            <Notification title="Error">
+                                No se puede actualizar la suscripción porque aún
+                                está vigente.
+                            </Notification>,
+                        )
+                        return // Salir de la función sin hacer nada
+                    }
+                }
+
+                // Obtener la fecha actual (fecha_inicio)
+                const fechaInicio = new Date()
+
+                // Calcular la fecha_fin sumando los días de vigencia
+                const vigenciaDias = parseInt(
+                    selectedPerson.subscripcion.vigencia,
+                    10,
+                ) // Asumiendo que la vigencia es un número de días
+                const fechaFin = new Date(fechaInicio)
+                fechaFin.setDate(fechaInicio.getDate() + vigenciaDias) // Sumar los días de vigencia
+
+                // Referencia al documento del usuario en la colección Usuarios
+                const userDoc = doc(db, 'Usuarios', selectedPerson.usuario.id)
+
+                // Actualizar los campos en Firestore
+                await updateDoc(userDoc, {
+                    'subscripcion_actual.status':
+                        selectedPerson.subscripcion.status,
+                    'subscripcion_actual.monto':
+                        selectedPerson.subscripcion.monto,
+                    'subscripcion_actual.vigencia':
+                        selectedPerson.subscripcion.vigencia,
+                    'subscripcion_actual.fecha_inicio': fechaInicio, // Fecha de inicio (actual)
+                    'subscripcion_actual.fecha_fin': fechaFin, // Fecha de fin calculada
+                })
+
+                toast.push(
+                    <Notification title="Éxito">
+                        Estado de suscripción actualizado con éxito.
+                    </Notification>,
+                )
+
+                // Actualizar el estado local de usuarios
+                setUsuarios((prevUsuarios) => {
+                    return prevUsuarios.map((usuario) => {
+                        if (usuario.id === selectedPerson.usuario?.id) {
+                            return {
+                                ...usuario,
+                                subscripcion_actual:
+                                    selectedPerson.subscripcion,
+                            }
+                        }
+                        return usuario
+                    })
+                })
+
+                setDrawerIsOpen(false) // Cierra el drawer
+            } catch (error) {
+                console.error(
+                    'Error actualizando el estado de la suscripción:',
+                    error,
+                )
+                toast.push(
+                    <Notification title="Error">
+                        Hubo un error al actualizar el estado de la suscripción.
+                    </Notification>,
+                )
+            }
+        } else {
+            toast.push(
+                <Notification title="Error">
+                    Información de usuario o suscripción no disponible.
+                </Notification>,
+            )
+        }
+    }
+
+    const formatDate = (timestamp: unknown): string => {
+        if (timestamp instanceof Timestamp) {
+            const dateObj = timestamp.toDate() // Convierte Timestamp a Date
+            return dateObj.toLocaleDateString('es-ES') // Formatea la fecha en formato 'DD/MM/YYYY'
+        }
+        console.warn('Timestamp no válido:', timestamp)
+        return '-' // Si no es un Timestamp, muestra un guión
+    }
 
     const columns: ColumnDef<Usuario>[] = [
         {
@@ -75,15 +211,11 @@ const UsuariosComponent = () => {
             accessorKey: 'nombre',
         },
         {
-            header: 'Status',
-            accessorKey: 'subscripcion_actual.status',
-        },
-        {
             header: 'Monto',
             accessorKey: 'subscripcion_actual.monto',
             filterFn: (row, columnId, value) => {
                 const monto = row.getValue(columnId)
-                return monto?.toString().includes(value) || false // Asegúrate de devolver siempre true o false
+                return monto?.toString().includes(value) || false
             },
             cell: ({ row }: { row: Row<Usuario> }) => {
                 const monto = parseFloat(
@@ -95,10 +227,72 @@ const UsuariosComponent = () => {
         {
             header: 'Fecha de Aprobacion',
             accessorKey: 'subscripcion_actual.fecha_inicio',
+            cell: ({ row }) => {
+                const fechaInicio =
+                    row.original.subscripcion_actual?.fecha_inicio
+                console.log('Fecha inicio:', fechaInicio) // Verifica aquí la salida
+                return fechaInicio ? formatDate(fechaInicio) : '-'
+            },
         },
         {
             header: 'Vigente hasta',
             accessorKey: 'subscripcion_actual.fecha_fin',
+            cell: ({ row }) => {
+                console.log(
+                    'Fecha de fin:',
+                    row.original.subscripcion_actual?.fecha_fin,
+                )
+                const fechaFin = row.original.subscripcion_actual?.fecha_fin
+                return fechaFin ? formatDate(fechaFin) : '-'
+            },
+        },
+        {
+            header: 'Status',
+            accessorKey: 'subscripcion_actual.status',
+            cell: ({ getValue }) => {
+                const status = (getValue() as string) || 'N/A' // Asegura que status es un string
+
+                let icon = null
+                let color = ''
+
+                if (status === 'Aprobado') {
+                    icon = <FaCheckCircle className="mr-1" />
+                    color = 'text-green-500'
+                } else if (status === 'Por Aprobar') {
+                    icon = <FaExclamationCircle className="mr-1" />
+                    color = 'text-yellow-500'
+                } else if (status === 'Rechazado') {
+                    color = 'text-red-500'
+                }
+
+                return (
+                    <div className={`flex items-center ${color}`}>
+                        {icon}
+                        <span>{status}</span>
+                    </div>
+                )
+            },
+        },
+        {
+            header: ' ',
+            cell: ({ row }) => {
+                const subscription = row.original.subscripcion_actual
+
+                return (
+                    <div className="gap-2">
+                        {subscription ? ( // Solo muestra el botón si subscription está definido
+                            <button
+                                onClick={() =>
+                                    openDrawer(row.original, subscription)
+                                }
+                                className="text-blue-900"
+                            >
+                                <FaRegEye />
+                            </button>
+                        ) : null}
+                    </div>
+                )
+            },
         },
     ]
 
@@ -120,12 +314,9 @@ const UsuariosComponent = () => {
     const startIndex = (currentPage - 1) * rowsPerPage
     const endIndex = startIndex + rowsPerPage
 
-    const handlePaginationChange = (page: number) => {
-        setCurrentPage(page)
-    }
+    const handlePaginationChange = (page: number) => setCurrentPage(page)
 
     const renderContent = () => {
-        if (loading) return <p>Cargando usuarios...</p>
         if (error) return <p>{error}</p>
 
         return (
@@ -221,24 +412,12 @@ const UsuariosComponent = () => {
                             ))}
                     </Table.TBody>
                 </Table>
-                <div className="pagination">
-                    {Array.from(
-                        { length: Math.ceil(totalRows / rowsPerPage) },
-                        (_, index) => (
-                            <button
-                                key={index}
-                                onClick={() =>
-                                    handlePaginationChange(index + 1)
-                                }
-                                className={
-                                    currentPage === index + 1 ? 'active' : ''
-                                }
-                            >
-                                {index + 1}
-                            </button>
-                        ),
-                    )}
-                </div>
+                <Pagination
+                    onChange={handlePaginationChange}
+                    currentPage={currentPage}
+                    totalRows={totalRows}
+                    rowsPerPage={rowsPerPage}
+                />
             </>
         )
     }
@@ -247,6 +426,103 @@ const UsuariosComponent = () => {
         <div style={{ padding: '20px' }}>
             <h2>Suscripciones Actuales</h2>
             {renderContent()}
+            <Drawer
+                isOpen={drawerIsOpen}
+                onClose={closeDrawer}
+                className="rounded-md shadow"
+            >
+                <div className="grid grid-cols-2">
+                    <h2 className="flex mb-4 text-xl font-bold">
+                        Revisión Subscripción
+                    </h2>
+                    <div className="flex items-center">
+                        <Switcher
+                            // Compara con el estado actual de la subscripción
+                            defaultChecked={
+                                selectedPerson?.subscripcion?.status ===
+                                'Aprobado'
+                            }
+                            onChange={(e) => {
+                                setSelectedPerson((prev) => ({
+                                    ...prev, // Copia el objeto anterior
+                                    subscripcion: prev?.subscripcion
+                                        ? {
+                                              ...prev.subscripcion, // Copia de la subscripción actual
+                                              status: e
+                                                  ? 'Aprobado'
+                                                  : 'Por Aprobar', // Cambia el estado
+                                          }
+                                        : null, // Si no hay subscripción, se pone null (aunque deberías manejar este caso)
+                                }))
+                            }}
+                            color={
+                                selectedPerson?.subscripcion?.status ===
+                                'Aprobado'
+                                    ? 'green-500'
+                                    : 'red-500'
+                            }
+                        />
+                        <span className="ml-2 text-gray-700">
+                            {selectedPerson?.subscripcion?.status}
+                        </span>
+                    </div>
+                </div>
+                <div className="flex flex-col space-y-6">
+                    <label className="flex flex-col">
+                        <span className="font-semibold text-gray-700">
+                            Plan:
+                        </span>
+                        <input
+                            type="text"
+                            value={selectedPerson?.subscripcion?.nombre || ''}
+                            readOnly // Aquí se agrega el atributo readOnly
+                            className="mt-1 p-3 border border-gray-300 rounded-lg bg-gray-100 cursor-not-allowed" // Se añade cursor-not-allowed para indicar que no se puede editar
+                        />
+                    </label>
+                    <label className="flex flex-col">
+                        <span className="font-semibold text-gray-700">
+                            Taller Subscrito:
+                        </span>
+                        <input
+                            type="text"
+                            value={selectedPerson?.usuario?.nombre || ''}
+                            readOnly // Aquí se agrega el atributo readOnly
+                            className="mt-1 p-3 border border-gray-300 rounded-lg bg-gray-100 cursor-not-allowed" // Se añade cursor-not-allowed para indicar que no se puede editar
+                        />
+                    </label>
+                    <label className="flex flex-col">
+                        <span className="font-semibold text-gray-700">
+                            Monto
+                        </span>
+                        <input
+                            type="text"
+                            value={selectedPerson?.subscripcion?.monto || ''}
+                            readOnly // Aquí se agrega el atributo readOnly
+                            className="mt-1 p-3 border border-gray-300 rounded-lg bg-gray-100 cursor-not-allowed" // Se añade cursor-not-allowed para indicar que no se puede editar
+                        />
+                    </label>
+                    <label className="flex flex-col">
+                        <span className="font-semibold text-gray-700">
+                            Vigencia:
+                        </span>
+                        <input
+                            type="text"
+                            value={selectedPerson?.subscripcion?.vigencia || ''}
+                            readOnly // Aquí se agrega el atributo readOnly
+                            className="mt-1 p-3 border border-gray-300 rounded-lg bg-gray-100 cursor-not-allowed" // Se añade cursor-not-allowed para indicar que no se puede editar
+                        />
+                    </label>
+                </div>
+                <div className="text-center mt-6 ">
+                    <Button
+                        onClick={handleSaveChanges}
+                        style={{ backgroundColor: '#000B7E' }}
+                        className="text-white hover:opacity-80"
+                    >
+                        Guardar Cambios
+                    </Button>
+                </div>
+            </Drawer>
         </div>
     )
 }
