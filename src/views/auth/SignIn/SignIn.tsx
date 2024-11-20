9f30fbc5f0ead458145410427bbfa134edf23cab
Anthonychacon2412 <chaconanthony.la@gmail.com>
2024-11-20 14:28:36 -0400

    logo de inicio

--- src/views/auth/SignIn/SignIn.tsx
+++ src/views/auth/SignIn/SignIn.tsx
@@ -4,6 +4,13 @@ const SignIn = () => {
     return (
         <>
             <div className="mb-8 text-center">
+                <div className="flex justify-center m-3">
+                    <img
+                        src="/img/logo/logo-light-streamline.png"
+                        alt=""
+                        className="w-24 h-24"
+                    />
+                </div>
                 <h3 className="mb-1">Bienvenido a Solvers!</h3>
             </div>
             <SignInForm disableSubmit={false} />
