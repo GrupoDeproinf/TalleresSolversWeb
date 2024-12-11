460f25f7b46dca40acd4b37ba409e87ab7a45719
Anthonychacon2412 <chaconanthony.la@gmail.com>
2024-12-11 13:42:06 -0400

    mapa y whatsapp

--- src/views/pages/ProfileGarage/Components/MapsProfile.tsx
+++ src/views/pages/ProfileGarage/Components/MapsProfile.tsx
@@ -1,31 +1,34 @@
 import { GoogleMap, Marker } from '@react-google-maps/api'
 
-const MapsProfile = () => {
-    const center = { lat: 10.47915, lng: -66.90618 } // Centro predeterminado del mapa
-    const zoom = 10 // Nivel de zoom predeterminado
+interface MarkerData {
+    lat: number
+    lng: number
+}
 
-    // Ejemplo de marcadores predeterminados
-    const markers = [
-        { id: 1, lat: 10.47915, lng: -66.90618, title: 'Marcador 1' },
-        { id: 2, lat: 10.48015, lng: -66.90518, title: 'Marcador 2' },
-    ]
+interface MapsProfileProps {
+    markers: MarkerData[]
+    center: { lat: number; lng: number } // Prop para el centro dinámico
+}
 
+const MapsProfile: React.FC<MapsProfileProps> = ({ markers, center }) => {
     return (
-        <div style={{ height: '450px', width: '100%' }}>
+        <div style={{ height: '50%', width: '100%' }}>
             <GoogleMap
                 center={center}
-                zoom={zoom}
+                zoom={10}
                 mapContainerStyle={{
                     height: '400px',
                     width: '100%',
                     marginTop: '10px',
                 }}
             >
-                {markers.map((marker) => (
+                {markers.map((marker, index) => (
                     <Marker
-                        key={marker.id}
-                        position={{ lat: marker.lat, lng: marker.lng }}
-                        title={marker.title}
+                        key={index}
+                        position={{
+                            lat: marker.lat,
+                            lng: marker.lng,
+                        }}
                     />
                 ))}
             </GoogleMap>
